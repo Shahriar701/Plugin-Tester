2f50f124b23762f50d34f11be4db738244e5bcb0
Rezwan <shahriar.rezwan@nad.neura-robotics.com>
2023-11-10 14:33:06 +0100

    finished drag and drop zone

---
+++ src/components/RobotProject.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="Robot">
+    <div class="main-container">
+      <ProgramHeader text="Program" />
+      <div class="text-input">
+        <TextInputComponent text="Name" />
+      </div>
+      <div class="description-accordion">
+        <AccordionTextBoxComponent text="Description" />
+      </div>
+      <div>
+        <DropZoneHeaderComponent />
+      </div>
+      <div class="flex-grow">
+        <ComponentDropZone />
+      </div>
+      <div>
+        <DropZoneButtons first_button_text="Cancel" second_button_text="Save" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.Robot {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh; /* Set min-height to 100% of the viewport height */
+  background-color: #ecf0f3;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1200px;
+  background-color: #ecf0f3;
+  border: 1px solid #ccc;
+  height: 100vh;
+}
+.flex-grow {
+  flex: 1; /* Make the ComponentDropZone grow to fill the available vertical space */
+  min-height: 0; /* Allow the ComponentDropZone to shrink if necessary */
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.description-accordion {
+  padding: 12px;
+}
+
+.text-input {
+  padding: 12px;
+}</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import DynamicTreePlugin from 'dynamic-tree-plugin'
+import '../node_modules/bulma/css/bulma.css'
+import '../node_modules/bulma-extensions/dist/css/bulma-extensions.min.css'
+import '../node_modules/bulma-extensions/dist/js/bulma-extensions.min.js'
+import '../node_modules/dynamic-tree-plugin/src/core-style.scss'
+
+Vue.config.productionTip = false
+
+Vue.use(DynamicTreePlugin)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
